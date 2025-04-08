9b410052cc9e748f1770762c84ef228ecf364059
Tam Trang <tuo98490@temple.edu>
2025-04-08 11:03:29 -0400

    Modified button click for each game with its own function

--- src/App.js
+++ src/App.js
@@ -7,14 +7,27 @@ import Hangman from "./Hangman"
 
 function App() {
   const [game, setGame] = useState(null);
+
+  const play_TicTacToe = (string) => {
+    setGame(string);
+  }
+
+  const play_Snake = (string) => {
+    setGame(string);
+  }
+
+  const play_Hangman = (string) => {
+    setGame(string);
+  }
+
   return (
     <div className="App">
       {game === null ? (
         <div>
           <h1>MiniGames</h1>
-          <button onClick={() => setGame('ticTacToe')}>Tic-Tac-Toe</button>
-          <button onClick={() => setGame('snake')}>Snake</button>
-          <button onClick={() => setGame('hangman')}>Hangman</button>
+          <button onClick={() => play_TicTacToe("ticTacToe")}>Tic-Tac-Toe</button>
+          <button onClick={() => play_Snake("snake")}>Snake</button>
+          <button onClick={() => play_Hangman("hangman")}>Hangman</button>
         </div>
       ) : game === 'ticTacToe' ? (
         <TicTacToe setGame={setGame}/>
